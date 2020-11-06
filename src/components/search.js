a9bc7bbc7d278439c9bd3c92428186507f497c60
Bartłomiej Kiljan <bkiljan@apptension.com>
2020-11-06 09:41:54 +0100

    Improve search by limiting results, toggling search form etc

--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,12 @@
 import React, {useState} from "react";
 import {graphql, useStaticQuery, Link} from "gatsby";
 
+const SEARCH_RESULTS_LIMIT = 5;
+
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredData, setFilteredData] = useState([]);
+  const [toggleSearch, setToggleSearch] = useState(false);
 
   const { allWpPost, allWpPage } = useStaticQuery(graphql`
     query SearchData {
@@ -50,12 +53,14 @@ const Search = () => {
   ];
 
   const getFilteredData = (term) => {
-    setFilteredData(allData.filter(post =>{
+    const filterData = allData.filter(post =>{
       const content = post.content ? post.content.toLowerCase() : '';
 
       return post.title.toLowerCase().includes(term) || content.includes(term)
       }
-    ));
+    );
+    filterData.length = SEARCH_RESULTS_LIMIT;
+    setFilteredData(filterData)
   };
 
   const handleSearchInput = ({ currentTarget: { value } }) => {
@@ -70,14 +75,32 @@ const Search = () => {
 
   return (
     <>
-      <form>
-        <input title="search" value={searchTerm.toLowerCase()} onChange={(e) => handleSearchInput(e)}/>
-      </form>
-      { filteredData.length ?
-        <ul>
-          { filteredData.map(post => <li id={post.id}><Link to={`/blog/${post.slug}`}>{post.title} - {post.nodeType}</Link></li>) }
-        </ul>
-        : searchTerm.length > 0 && <p>No search results</p>
+      <button onClick={() => setToggleSearch(toggleSearch => !toggleSearch) }>
+        { toggleSearch ? 'Close search' : 'Open search' }
+      </button>
+      { toggleSearch &&
+        <div>
+          <form>
+            <input
+              name="search"
+              autoFocus={true}
+              autoComplete="false"
+              value={searchTerm.toLowerCase()}
+              onChange={(e) => handleSearchInput(e)}/>
+          </form>
+          { filteredData.length ?
+            <ul>
+              { filteredData.map(post =>
+                <li id={post.id}>
+                  <Link to={ post.nodeType === 'Page' ? `/${post.slug}` : `/blog/${post.slug}` }>
+                    {post.title} - {post.nodeType}
+                  </Link>
+                </li>)
+              }
+            </ul>
+            : searchTerm.length > 0 && <p>No search results</p>
+          }
+        </div>
       }
     </>
   );
