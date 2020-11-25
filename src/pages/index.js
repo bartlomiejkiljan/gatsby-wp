7408958d5af3a102c4d8bcd76a01434645839d89
Bartłomiej Kiljan <bkiljan@apptension.com>
2020-11-25 12:08:43 +0100

    WIP: query Gutenberg blocks data by GraphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,41 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import RecentPosts from "../components/recent-posts/recent-posts";
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
+    Content: { data.allWpPage.nodes[0].blocks.length }
+    <div dangerouslySetInnerHTML={{ __html: data.allWpPage.nodes[0].content }} />
     <SEO title="Home" />
     <RecentPosts quantity={3} />
   </Layout>
 );
 
 export default IndexPage
+
+export const query = graphql`
+query Blocks {
+  allWpPage(filter: {title: {eq: "Home"}}) {
+    nodes {
+      content
+      blocks {
+        name
+        originalContent
+        saveContent
+        ... on WpCustomCtaBlock {
+          dynamicContent
+          originalContent
+          name
+          attributes {
+            buttonText
+            buttonUrl
+            className
+            title
+          }
+          saveContent
+          isDynamic
+        }
+      }
+      title
+    }
+  }
+}`;
