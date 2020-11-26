efa1b0985deca5343a4954df689450bf421489c8
Bartłomiej Kiljan <bkiljan@apptension.com>
2020-11-26 15:40:29 +0100

    Display CTA gutenberg content by gatsby component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 import React from "react"
+import { graphql } from 'gatsby'
+import WPGBlocks from "react-gutenberg"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import RecentPosts from "../components/recent-posts/recent-posts";
+import Cta from "../components/cta";
 
 const IndexPage = ({ data }) => (
   <Layout>
     Content: { data.allWpPage.nodes[0].blocks.length }
+    {data.allWpPage.nodes[0].blocks.map(block => {
+      if (block.name === 'custom/cta') {
+        return <Cta attributes={block.attributes} />;
+      }
+    })}
+    <WPGBlocks blocks={data.allWpPage.nodes[0].blocks} />
     <div dangerouslySetInnerHTML={{ __html: data.allWpPage.nodes[0].content }} />
     <SEO title="Home" />
     <RecentPosts quantity={3} />
