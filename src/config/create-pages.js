7b053cfb12c9704eb9fa0132680df2153898ab34
Bartłomiej Kiljan <bkiljan@apptension.com>
2020-12-09 14:53:24 +0100

    Create graphql fragment for blocks and use it on homepage

--- src/config/create-pages.js
+++ src/config/create-pages.js
@@ -9,7 +9,67 @@ module.exports = async ({ graphql, actions }) => {
         nodes {
           slug
           title
-          content
+          blocks {
+            name
+            ... on WpCustomCtaBlock {
+              attributes {
+                buttonText
+                buttonUrl
+                className
+                title
+              }
+            }
+            ... on WpCustomQuoteBlock {
+              attributes {
+                author
+                className
+                company
+                logoAlt
+                logoUrl
+                text
+              }
+            }
+            ... on WpCoreParagraphBlock {
+              attributes {
+                ... on WpCoreParagraphBlockAttributes {
+                  content
+                }
+              }
+            }
+            ... on WpCustomHeroSectionBlock {
+              saveContent
+              innerBlocks {
+                name
+                ... on WpCustomHeroItemBlock {
+                  name
+                  attributes {
+                    title
+                    bgColor
+                    bgType
+                    bgUrl
+                    buttonText
+                    buttonUrl
+                    caption
+                    imageAlign
+                    imageAlt
+                    imageUrl
+                  }
+                }
+              }
+            }
+            ... on WpCustomCardSectionBlock {
+              name
+              innerBlocks {
+                name
+                ... on WpCustomCardBlock {
+                  attributes {
+                    desc
+                    title
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
