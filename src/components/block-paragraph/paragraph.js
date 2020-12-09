16839f23cde3e04e98dda6d1a6573ee34c424966
Bartłomiej Kiljan <bkiljan@apptension.com>
2020-12-09 10:55:52 +0100

    Use env variables

---
+++ src/components/block-paragraph/paragraph.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Paragraph = ({ attributes: {content} }) => {
+
+  return (
+    <p>{content}</p>
+  );
+};
+
+export default Paragraph;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,64 @@
 import React from "react"
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import RecentPosts from "../components/recent-posts/recent-posts";
+import Cta from "../components/block-cta/cta";
+import Paragraph from "../components/block-paragraph/paragraph";
+import Quote from "../components/block-quote/quote";
 
-const IndexPage = () => (
-  <Layout title="Home">
+const IndexPage = ({ data }) => {
+  console.log(data.allWpPage.nodes[0].blocks);
+  return (
+    <Layout title="Home">
+    {data.allWpPage.nodes[0].blocks.map(block => {
+      if (block.name === 'custom/cta') {
+        return <Cta attributes={block.attributes} />;
+      } else if (block.name === 'core/paragraph') {
+        return <Paragraph attributes={block.attributes} />
+      } else if (block.name === 'custom/quote') {
+        return <Quote attributes={block.attributes} />
+      }
+    })}
     <RecentPosts quantity={3} />
   </Layout>
-);
+)};
 
 export default IndexPage
+
+export const query = graphql`
+query Blocks {
+  allWpPage(filter: {title: {eq: "Home"}}) {
+    nodes {
+      title
+      blocks {
+        name
+        ... on WpCustomCtaBlock {
+          attributes {
+            buttonText
+            buttonUrl
+            className
+            title
+          }
+        }
+        ... on WpCustomQuoteBlock {
+          attributes {
+            author
+            className
+            company
+            logoAlt
+            logoUrl
+            text
+          }
+        }
+        ... on WpCoreParagraphBlock {
+          attributes {
+            ... on WpCoreParagraphBlockAttributes {
+              content
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
