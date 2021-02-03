2df7493d3bcaa215396bb7a1a16139b05bf1ed6e
Bartłomiej Kiljan <bkiljan@apptension.com>
2021-02-03 13:05:06 +0100

    Add styled component to cta gutenberg block

--- src/components/block-cta/cta.js
+++ src/components/block-cta/cta.js
@@ -1,12 +1,14 @@
 import React from "react"
+import {CtaTitle,CtaWrapper} from "./cta-styles";
+import {Button} from "../../styles/theme";
 
 const Cta = ({ attributes: {title, buttonUrl, buttonText} }) => {
 
   return (
-    <div className="wp-block-custom-cta">
-      <h3 className="cta__title">{title}</h3>
-      <a className="button button--primary" href={buttonUrl}>{buttonText}</a>
-    </div>
+    <CtaWrapper>
+      <CtaTitle>{title}</CtaTitle>
+      <Button href={buttonUrl}>{buttonText}</Button>
+    </CtaWrapper>
   );
 };
 
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -12,3 +12,23 @@ export const Container = styled.div`
   margin: 0 auto;
   max-width: 960px;
 `;
+
+export const Button = styled.a`
+  display: inline-block;
+  padding: 10px 16px;
+  border: none;
+  text-decoration: none;
+  transition: background-color .35s ease;
+  cursor: pointer;
+  background-color: ${({theme}) => theme.primary};
+  color: ${({theme}) => theme.white};
+
+  &:focus {
+    outline: none;
+  }
+  
+  &:hover {
+    background-color: ${({theme}) => theme.secondary};
+    color: ${({theme}) => theme.white};
+  }
+`;
